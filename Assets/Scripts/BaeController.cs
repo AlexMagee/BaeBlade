73f5a7104485be1d600d645573aa5aeaff541cbd
Alex Magee <Alex.Magee1997@gmail.com>
2025-01-12 23:22:13 -0600

    Added controls
    Use left and right or A and D to turn.
    Drifts a bit. Might need fine tuning

--- Assets/Scripts/BaeController.cs
+++ Assets/Scripts/BaeController.cs
@@ -1,15 +1,19 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
+[RequireComponent(typeof(PlayerInput))]
 public class BaeController : MonoBehaviour
 {
+    public Transform mesh;
+    PlayerInput playerInput;
     public float speed = 1f;
-    public float turn_speed = 1f;
+    public float turnSpeed = 1f;
     Rigidbody rb;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         rb = GetComponent<Rigidbody>();
-        
+        playerInput = GetComponent<PlayerInput>();
     }
 
     // Update is called once per frame
@@ -20,11 +24,12 @@ public class BaeController : MonoBehaviour
 
     void FixedUpdate()
     {
+        Vector2 moveDirection = playerInput.actions["move"].ReadValue<Vector2>();
+
         // get horizontal linearvelity magnitude
         float hm = new Vector3(rb.linearVelocity.x, 0, rb.linearVelocity.z).magnitude;
         rb.AddRelativeForce(Vector3.forward * (speed - hm), ForceMode.VelocityChange);
 
-        // apply torque to turn
-        rb.angularVelocity = new Vector3(0, turn_speed, 0);
+        rb.angularVelocity = new Vector3(0, moveDirection.x * turnSpeed, 0);
     }
 }
