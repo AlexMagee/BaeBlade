72bbacf01d7032f34f0224e69acd8c9adcbe3115
Alex Magee <Alex.Magee1997@gmail.com>
2025-01-14 00:33:40 -0600

    Reworked movement and added bouncy obstacles

---
+++ Assets/BouncyObstacle.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+
+public class BouncyObstacle : MonoBehaviour
+{
+    public float bounciness = 1f;
+    public float up = 1f;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        Vector3 explosionCenter = collision.contacts[0].point;
+        float explosionRadius = 5f;
+
+        // Apply explosion force, but only in the horizontal direction (x, z)
+        Vector3 direction = (collision.transform.position - explosionCenter).normalized;
+        direction.y = up; // Ignore vertical component
+
+        Debug.Log(explosionCenter + ", " + direction);
+
+        // Apply the horizontal force
+        collision.rigidbody.AddForce(direction * bounciness, ForceMode.Impulse);
+        collision.transform.GetComponent<BaeController>().grounded = false;
+        collision.transform.GetComponent<AudioSource>().Play();
+        // collision.rigidbody.AddForce((-collision.relativeVelocity.normalized + (Vector3.up * up)) * bounciness, ForceMode.Impulse);
+        // collision.rigidbody.AddExplosionForce(bounciness, collision.contacts[0].point, 5);
+    }
+}
--- Assets/Scripts/BaeController.cs
+++ Assets/Scripts/BaeController.cs
@@ -9,6 +9,9 @@ public class BaeController : MonoBehaviour
     public float speed = 1f;
     public float turnSpeed = 1f;
     Rigidbody rb;
+
+    public bool cap = true;
+    public bool grounded = true;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -22,14 +25,50 @@ public class BaeController : MonoBehaviour
         
     }
 
-    void FixedUpdate()
+    public float force = 10f;  // Energy output of the "flywheel"
+    public float maxSpeed = 5f;       // Maximum speed (to cap runaway effects)
+    public float stabilizationFactor = 0.5f; // Damping to resist external speed changes
+
+    private void FixedUpdate()
     {
         Vector2 moveDirection = playerInput.actions["move"].ReadValue<Vector2>();
 
-        // get horizontal linearvelity magnitude
-        float hm = new Vector3(rb.linearVelocity.x, 0, rb.linearVelocity.z).magnitude;
-        rb.AddRelativeForce(Vector3.forward * (speed - hm), ForceMode.VelocityChange);
+        // Get the velocity in the horizontal plane (ignore vertical component)
+        Vector3 horizontalVelocity = new Vector3(rb.linearVelocity.x, 0, rb.linearVelocity.z);
+
+        // Project the horizontal velocity onto the forward direction
+        float forwardSpeed = Vector3.Dot(horizontalVelocity, transform.forward);
+
+        // Stabilize speed (resist sudden changes from external forces)
+        float speedError = maxSpeed - Mathf.Abs(forwardSpeed);
+        Vector3 stabilizationForce = transform.forward * (speedError * stabilizationFactor);
+
+        // Apply constant forward force
+        Vector3 flywheelForce = transform.forward * force;
+
+        // Combine forces: constant propulsion + stabilization
+        if(grounded)
+            rb.AddForce(flywheelForce + stabilizationForce, ForceMode.Force);
+
+        // Cap the horizontal speed to avoid runaway motion
+        if (grounded && horizontalVelocity.magnitude > maxSpeed)
+        {
+            // Scale horizontal velocity to the maxSpeed
+            Vector3 cappedVelocity = horizontalVelocity.normalized * maxSpeed;
+
+            // Preserve the original vertical velocity
+            rb.linearVelocity = new Vector3(cappedVelocity.x, rb.linearVelocity.y, cappedVelocity.z);
+        }
 
         rb.angularVelocity = new Vector3(0, moveDirection.x * turnSpeed, 0);
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if(collision.contacts[0].point.y < transform.position.y)
+        {
+            grounded = true;
+            GetComponent<AudioSource>().Play();
+        }
     }
 }
