e08ce05dabdcaa3f9299c67dd8de69dee97d3c83
Alex Magee <Alex.Magee1997@gmail.com>
2025-01-15 00:05:35 -0600

    Added Boss Core and Soft Spots

--- Assets/BouncyObstacle.cs
+++ Assets/BouncyObstacle.cs
@@ -1,9 +1,11 @@
 using UnityEngine;
+using UnityEngine.Events;
 
 public class BouncyObstacle : MonoBehaviour
 {
     public float bounciness = 1f;
     public float up = 1f;
+    public UnityEvent hitEvent;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -25,13 +27,13 @@ public class BouncyObstacle : MonoBehaviour
         Vector3 direction = (collision.transform.position - explosionCenter).normalized;
         direction.y = up; // Ignore vertical component
 
-        Debug.Log(explosionCenter + ", " + direction);
-
         // Apply the horizontal force
         collision.rigidbody.AddForce(direction * bounciness, ForceMode.Impulse);
         collision.transform.GetComponent<BaeController>().grounded = false;
         collision.transform.GetComponent<AudioSource>().Play();
         // collision.rigidbody.AddForce((-collision.relativeVelocity.normalized + (Vector3.up * up)) * bounciness, ForceMode.Impulse);
         // collision.rigidbody.AddExplosionForce(bounciness, collision.contacts[0].point, 5);
+
+        hitEvent.Invoke();
     }
 }
