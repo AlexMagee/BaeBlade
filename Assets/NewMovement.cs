bf7a921db674fa3fb62ee1ce80411f6d409c2a6b
Alex Magee <Alex.Magee1997@gmail.com>
2025-01-26 16:59:45 -0600

    Integrated gamepad

--- Assets/NewMovement.cs
+++ Assets/NewMovement.cs
@@ -1,15 +1,23 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class NewMovement : MonoBehaviour
 {
-    public float dir = 0f;
     public float spinSpeed = 0f;
     public float moveSpeed = 0f;
 
     void Update()
     {
+        // Get gamepad reference
+        Gamepad gamepad = Gamepad.current;
+        if(gamepad == null)
+        {
+            return;
+        }
+        // Get move value
+        Vector2 move = gamepad.leftStick.ReadValue();
         // Make rotation
-        transform.Rotate(Vector3.up * spinSpeed * dir * Time.deltaTime, Space.Self);
+        transform.Rotate(Vector3.up * spinSpeed * move.x * Time.deltaTime, Space.Self);
         // Calculate forward motion
         Vector3 motion = Vector3.forward * moveSpeed * Time.deltaTime;
         // Apply forward motion
